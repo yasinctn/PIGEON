7325156a5e8f187314ab1dc6cc8ee14b5ba666ff
Yasin <yasincetin2014@gmail.com>
2023-05-25 20:27:25 +0300

    Protocols and AuthService created

---
+++ Pigeon/Services/FirebaseAuthService.swift
@@ -0,0 +1,47 @@
+//
+//  FirebaseAuthService.swift
+//  Pigeon
+//
+//  Created by Yasin Cetin on 25.05.2023.
+//
+
+import Foundation
+import Firebase
+
+protocol AuthServiceProtocol {
+    func login(email: String, password: String, completion: @escaping (Error?) -> Void)
+    func register(email: String, password: String, completion: @escaping (Error?) -> Void)
+    func logout(completion: @escaping (NSError?) -> Void)
+}
+
+final class FirebaseAuthService {
+    
+    let currentUser = Auth.auth().currentUser?.email
+    
+}
+
+extension FirebaseAuthService: AuthServiceProtocol {
+    
+    func login(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            completion(error)
+        }
+    }
+    
+    func register(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            completion(error)
+        }
+    }
+    
+    func logout(completion: @escaping (NSError?) -> Void) {
+        do{
+            try Auth.auth().signOut()
+            completion(nil)
+        }
+        catch let error as NSError{
+            completion(error)
+        }
+        
+    }
+}
