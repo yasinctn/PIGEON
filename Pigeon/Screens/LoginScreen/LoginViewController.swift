ceaf2e7e5ee4123aa2827f317e34dab814c5f452
Yasin <yasincetin2014@gmail.com>
2023-05-25 20:30:32 +0300

    login feature completed

--- Pigeon/Screens/LoginScreen/LoginViewController.swift
+++ Pigeon/Screens/LoginScreen/LoginViewController.swift
@@ -7,20 +7,49 @@
 
 import UIKit
 
+protocol LoginViewInput: AnyObject {
+    func showAlert(_ message: String)
+    func showAlert(with identifier: String, title: String, actionTitle: String)
+}
+
 final class LoginViewController: UIViewController {
 
     @IBOutlet private weak var emailTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
     @IBOutlet private weak var loginButton: UIButton!
     
+    private var viewModel: LoginViewOutput?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel = LoginViewModel(view: self)
+        setupUI()
+        
     }
     
     @IBAction private func loginButtonTapped(_ sender: UIButton) {
+        viewModel?.login(email: emailTextField.text, password: passwordTextField.text)
     }
     
     
+}
+// MARK: - LoginViewInput
+
+extension LoginViewController: LoginViewInput, AlertPresentable {
+    func showAlert(_ message: String) {
+        presentAlert(message)
+    }
+    
+    func showAlert(with identifier: String, title: String, actionTitle: String) {
+        presentAlertWithAction(identifier: identifier, alertTitle: title, actionTitle: actionTitle)
+    }
+}
+
+// MARK: - Private Methods
+
+private extension LoginViewController {
+    
+    func setupUI() {
+        loginButton.layer.cornerRadius = 20
+    }
 }
--- Pigeon/Screens/LoginScreen/LoginViewModel.swift
+++ Pigeon/Screens/LoginScreen/LoginViewModel.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+
+protocol LoginViewOutput: AnyObject {
+    func login(email: String?, password: String?)
+}
+
+final class LoginViewModel {
+    private weak var view: LoginViewInput?
+    private let authService: AuthServiceProtocol?
+    
+    init(view: LoginViewInput) {
+        self.view = view
+        self.authService = FirebaseAuthService()
+    }
+    
+}
+
+// MARK: - LoginViewOutput
+
+extension LoginViewModel: LoginViewOutput {
+    
+    func login(email: String?, password: String?) {
+        let (isValid, warning) = checkValidation(email: email, password: password)
+        guard let email, let password, isValid else {
+            view?.showAlert(warning ?? "")
+            return
+        }
+        
+        authService?.login(email: email, password: password) { [weak self] authError in
+            guard let self else { return }
+            
+            if let error = authError {
+                self.view?.showAlert(error.localizedDescription)
+            }else {
+                self.view?.showAlert(with: "loginToChat", title: "Login Success", actionTitle: "Go To Conversations")
+                
+            }
+        }
+        
+    }
+    
+    
+}
+
+// MARK: - Private Methods
+
+private extension LoginViewModel {
+    
+    func checkValidation(email: String?,
+                                 password: String?) -> (Bool, String?) {
+        
+        guard let email, let password else {
+            return (false, "Please fill fields")
+        }
+        if email.isEmpty {
+            return (false, "e mail empty \nPlease type an e mail")
+        } else if password.isEmpty {
+            return (false, "password empty \nPlease type a pasword")
+        }
+        return (true, nil)
+    }
+}
+
