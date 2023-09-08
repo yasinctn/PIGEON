ba36e8e3e76978300e00bb98cbeb843465aa26dc
Yasin <yasincetin2014@gmail.com>
2023-09-08 18:02:59 +0300

    chat feature completed

--- Pigeon/Screens/ChatScreen/ChatViewController.swift
+++ Pigeon/Screens/ChatScreen/ChatViewController.swift
@@ -7,18 +7,81 @@
 
 import UIKit
 
+protocol ChatViewInput: AnyObject {
+    func reloadData()
+    func showAlert(_ message: String)
+    func clearMessageTextField()
+}
+
 final class ChatViewController: UIViewController {
 
     @IBOutlet private weak var messagesTableView: UITableView!
     @IBOutlet private weak var messageTextField: UITextField!
     @IBOutlet private weak var sendButton: UIButton!
     
+    private var viewModel: ChatViewOutput?
+    var currentConversation: Conversation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel = ChatViewModel(view: self)
+        prepareTableView()
+        viewModel?.getMessages(conversationId: currentConversation?.conversationID)
     }
     
     @IBAction private func sendButtonTapped(_ sender: UIButton) {
+        viewModel?.sendMessages(message: messageTextField.text, conversation: currentConversation)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.messageCount ?? .zero
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MessageCell.identifier, for: indexPath) as! MessageCell
+        if let message = viewModel?.getMessage(indexPath.row) {
+            cell.configure(for: MessageCellPresenter(messageLabel: message.body, date: message.date, receiver: message.receiver, sender: message.sender), currentUser: viewModel?.currentUser)
+        }
+        return cell
+    }
+    
+    
+}
+// MARK: - UITableViewDelegate
+
+extension ChatViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - ChatViewInput
+
+extension ChatViewController: ChatViewInput, AlertPresentable {
+    func clearMessageTextField() {
+        messageTextField.text = nil
+    }
+    
+    
+    func showAlert(_ message: String) {
+        presentAlert(message)
+    }
+    
+    
+    func reloadData() {
+        messagesTableView.reloadData()
+    }
+}
+
+private extension ChatViewController {
+    
+    func prepareTableView() {
+        messagesTableView.dataSource = self
+        messagesTableView.delegate = self
+        messagesTableView.separatorStyle = .none
+        messagesTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "MessageCell")
     }
 }
--- Pigeon/Screens/ChatScreen/ChatViewModel.swift
+++ Pigeon/Screens/ChatScreen/ChatViewModel.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+
+protocol ChatViewOutput {
+    func getMessages(conversationId: String?)
+    func getMessage(_ index: Int) -> Message?
+    func sendMessages(message: String?, conversation: Conversation?)
+    var messageCount: Int? { get }
+    var currentUser: String? { get }
+}
+
+final class ChatViewModel {
+    
+    weak var view: ChatViewInput?
+    private var databaseService: DatabaseServiceProtocol?
+    private(set) var messageCount: Int?
+    private var messages : [Message] = []
+    private(set) var currentUser: String?
+    
+    init(view: ChatViewInput,
+         databaseService: DatabaseServiceProtocol = DatabaseService()) {
+        
+        self.view = view
+        self.databaseService = databaseService
+    }
+    
+}
+
+// MARK: - ChatViewOutput
+
+extension ChatViewModel: ChatViewOutput {
+    func getMessage(_ index: Int) -> Message? {
+        self.messages[safe: index]
+    }
+    
+    func getMessages(conversationId: String?) {
+        if let conversationId {
+            databaseService?.readMessageData(conversationId: conversationId, completion: { [weak self] allMessages, error, currentUser in
+                guard let self else { return }
+                
+                if let error = error {
+                    self.view?.showAlert(error.localizedDescription)
+                    
+                } else if let messages = allMessages {
+                    self.messages = []
+                    self.messages = messages
+                    self.messageCount = messages.count
+                    self.currentUser = currentUser
+                    
+                    DispatchQueue.main.async {
+                        self.view?.reloadData()
+                    }
+                }
+            })
+        }
+    }
+    
+    func sendMessages(message: String?, conversation: Conversation?) {
+        
+        let (isValid,warning) = checkValidation(message: message)
+        guard let conversation, let message, isValid else {
+            view?.showAlert(warning ?? "")
+            return
+        }
+        databaseService?.writeMessageData(users: conversation.users,
+                                          conversation: conversation,
+                                          message: message) { [weak self] error in
+            guard let self else { return }
+            if let error = error {
+                self.view?.showAlert(error.localizedDescription)
+            }else {
+                self.view?.clearMessageTextField()
+            }
+        }
+    }
+    
+    
+}
+
+private extension ChatViewModel {
+    
+    func checkValidation(message: String?) -> (Bool, String?) {
+        guard let message else {
+            return (false, "please type a message")
+        }
+        return (true, nil)
+    }
+}
+
