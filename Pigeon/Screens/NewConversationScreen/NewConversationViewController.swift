a72d521409f893f85c3cb56cc21227a686410dc9
Yasin <yasincetin2014@gmail.com>
2023-09-09 17:45:52 +0300

    new conversation creation feature added

--- Pigeon/Screens/NewConversationScreen/NewConversationViewController.swift
+++ Pigeon/Screens/NewConversationScreen/NewConversationViewController.swift
@@ -7,14 +7,76 @@
 
 import UIKit
 
+protocol NewConversationViewInput: AnyObject {
+    func reloadData()
+    func prepareTableView()
+    func popViewController()
+    func showAlert(_ message: String)
+}
+
 final class NewConversationViewController: UIViewController {
 
     @IBOutlet private weak var usersTableView: UITableView!
+    private var viewModel: NewConversationViewOutput?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        prepareTableView()
+        viewModel = NewConversationViewModel(view: self)
+        viewModel?.getUsers()
     }
-   
+}
+
+//MARK: - TableViewDataSource
+
+extension NewConversationViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel?.usersCount ?? .zero
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UserCell.identifier, for: indexPath) as! UserCell
+        if let user = viewModel?.getUser(indexPath.row) {
+            cell.configure(for: UserCellPresenter(username: user))
+        }
+        return cell
+    }
+    
+    
+}
+
+// MARK: - TableViewDelegate
+
+extension NewConversationViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel?.setSelectedUser(by: indexPath.row)
+        viewModel?.createNewConversation(selectedUser: viewModel?.selectedUser)
+        
+    }
+    
+}
+
+//MARK: - NewConversationViewInput
+
+extension NewConversationViewController: NewConversationViewInput, AlertPresentable {
+    
+    func popViewController() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func prepareTableView() {
+        usersTableView.dataSource = self
+        usersTableView.delegate = self
+    }
+    
+    func reloadData() {
+        usersTableView.reloadData()
+    }
+    
+    func showAlert(_ message: String) {
+        presentAlert(message)
+    }
+    
 }
--- Pigeon/Screens/NewConversationScreen/NewConversationViewModel.swift
+++ Pigeon/Screens/NewConversationScreen/NewConversationViewModel.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+
+protocol NewConversationViewOutput {
+    var usersCount: Int { get }
+    var selectedUser: String? { get }
+    func setSelectedUser(by index: Int)
+    func getUsers()
+    func getUser(_ index: Int) -> String?
+    func createNewConversation(selectedUser: String?)
+}
+
+final class NewConversationViewModel {
+    
+    weak var view: NewConversationViewInput?
+    private var databaseService: DatabaseServiceProtocol?
+    private var users: [String] = []
+    private(set) var selectedUser: String?
+    
+    init(view: NewConversationViewInput,
+         databaseService: DatabaseServiceProtocol? = DatabaseService()) {
+        
+        self.view = view
+        self.databaseService = databaseService
+    }
+}
+
+// MARK: - NewConversationViewOutput
+
+extension NewConversationViewModel: NewConversationViewOutput {
+
+    
+    func createNewConversation(selectedUser: String?) {
+        guard let selectedUser else { return }
+        databaseService?.createConversation(selectedUser: selectedUser ) { [weak self] conversation, error in
+            guard let self else { return }
+            if let error = error {
+                self.view?.showAlert(error.localizedDescription)
+            }else {
+                self.view?.popViewController()
+                self.view?.showAlert("Conversation Created")
+            }
+        }
+    }
+    
+    
+    func getUsers() {
+        
+        databaseService?.readUsers { [weak self] users, error in
+            guard let self else { return }
+            if let error = error {
+                self.view?.showAlert(error.localizedDescription)
+            }else if let users = users {
+                self.users = []
+                self.users = users
+                
+                DispatchQueue.main.async {
+                    self.view?.reloadData()
+                }
+            }
+        }
+    }
+    
+    
+    func setSelectedUser(by index: Int) {
+         guard let user = users[safe: index] else { return }
+         selectedUser = user
+     }
+     
+     
+    func getUser(_ index: Int) -> String? {
+        self.users[safe: index]
+    }
+    
+    var usersCount: Int {
+        users.count
+    }
+    
+}
