70d15d050101019518a40c0068c802db765c7a60
Yasin <yasincetin2014@gmail.com>
2023-09-13 00:53:08 +0300

    user model created

---
+++ Pigeon/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Pigeon
+//
+//  Created by Yasin Cetin on 9.09.2023.
+//
+
+import Foundation
+
+struct User {
+    let email: String
+    let username: String
+    
+    init(email: String, username: String) {
+        self.email = email
+        self.username = username
+    }
+}
